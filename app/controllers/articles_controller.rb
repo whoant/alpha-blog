6c2f42068d930dddf39a5c620b062eb8325922a6
whoant <vovanhoangtuan4.2@gmail.com>
2022-08-13 22:54:53 +0700

    Add logic to redirect if user login

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     @article.save
     if @article.save
       flash[:notice] = "Article was created successfully."
