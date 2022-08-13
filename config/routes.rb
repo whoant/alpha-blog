257848582605bdcacb3b82a7dc6ca998007e3684
whoant <vovanhoangtuan4.2@gmail.com>
2022-08-13 22:47:09 +0700

    Add authenticate helper method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   root 'pages#home'
   resources :articles
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
